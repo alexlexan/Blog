2395e1b0e5b097416b5255b65e9e75b46aecf325
Алексей <milutkin.work@gmail.com>
2020-10-25 00:39:59 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
+  target: "serverless",
   env: {
     accessToken: "ua-Ju74U90BjomcgNA9eV7dzKS2LkP0xAUjV85HwtB4",
     spaceId: "whq9f1y7vk0s",
-    target: "serverless",
   },
 };
