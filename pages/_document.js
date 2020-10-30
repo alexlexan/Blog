3e9cea8a58855e85726c18535cc4cf5729aae2c2
Алексей <milutkin.work@gmail.com>
2020-10-30 21:33:03 +0300

    update css

--- pages/_document.js
+++ pages/_document.js
@@ -2,15 +2,42 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class SiteDocument extends Document {
-  render() {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
-    const main = sheet.collectStyles(<Main />);
-    const styleTags = sheet.getStyleElement();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      // wraps the collectStyles provider around our <App />.
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      // extract the initial props that may be present.
+      const initialProps = await Document.getInitialProps(ctx);
+
+      // returning the original props together with our styled components.
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
     return (
       <Html>
-        <Head>{styleTags}</Head>
+        <Head>{this.props.styleTags}</Head>
         <body>
-          <div className="root">{main}</div>
+          <Main />
           <NextScript />
         </body>
       </Html>
